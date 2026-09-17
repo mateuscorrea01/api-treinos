4edf5b6c6ab974714760229e77e5f93d72c8c75f
mateuscorrea01 <mateus.correa01@escola.pr.gov.br>
2026-09-17 10:57:55 +0000

    aula 02 -  api treinos

--- servidor.js
+++ servidor.js
@@ -50,7 +50,7 @@ app.get('/ treinos ', (req, res) => {
 // GET /treinos/:id - busca um treino pelo id (404 se nao existir)
 // ------------------------------------------------------------
 
-app.get('/ treinos /: id ', (req, res) => {
+app.get('/treinos/:id', (req, res) => {
     const id = Number(req.params.id);
     const treino = treinos.find((t) => t.id === id);
     if (treino === undefined) {
@@ -63,7 +63,7 @@ app.get('/ treinos /: id ', (req, res) => {
 // POST /treinos - cria um treino (400 se os dados forem invalidos)
 // ------------------------------------------------------------
 
-app.post('/ treinos ', (req, res) => {
+app.post('/treinos', (req, res) => {
     const erro = validarTreino(req.body);
     if (erro !== null) {
         return res.status(400).json({ erro: erro });
@@ -82,7 +82,7 @@ app.post('/ treinos ', (req, res) => {
 // PUT /treinos/:id - substitui um treino
 // ------------------------------------------------------------
 
-app.put('/ treinos /: id ', (req, res) => {
+app.put('/treinos/:id ', (req, res) => {
     const id = Number(req.params.id);
     const treino = treinos.find((t) => t.id === id);
     if (treino === undefined) {
@@ -101,7 +101,7 @@ app.put('/ treinos /: id ', (req, res) => {
 // DELETE /treinos/:id - remove um treino
 // ------------------------------------------------------------
 
-app.delete('/ treinos /: id ', (req, res) => {
+app.delete('/treinos/:id ', (req, res) => {
     const id = Number(req.params.id);
     const posicao = treinos.findIndex((t) => t.id === id);
     if (posicao === -1) {
